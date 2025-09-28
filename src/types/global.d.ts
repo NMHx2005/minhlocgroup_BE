ba5e4888b4116a0ea50660b57fc08d454f6c7ca6
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2025-09-29 05:43:46 +0700

    Fix all TypeScript build errors - move all @types to dependencies

---
+++ src/types/global.d.ts
@@ -0,0 +1,16 @@
+// types/global.d.ts
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NODE_ENV: 'development' | 'production' | 'test';
+            PORT: string;
+            MONGODB_URI: string;
+            JWT_SECRET: string;
+            CLOUDINARY_CLOUD_NAME: string;
+            CLOUDINARY_API_KEY: string;
+            CLOUDINARY_API_SECRET: string;
+        }
+    }
+}
+
+export { };
